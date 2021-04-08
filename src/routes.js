6152d224868ac6d2a015017d078f40df1e885336
Fabiano César <fcfabiano.cesar@icloud.com>
2021-04-08 01:09:20 -0300

    EJS render

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,15 @@ const routes = api.Router();
 
 const views = __dirname + "/views/";
 
+const profile = {
+    name: "Fabiano",
+    avatar: "https://avatars.githubusercontent.com/u/47621123?v=4",
+    "monthly-budget": 10000,
+    "hours-per-day": 8,
+    "days-per-week": 6,
+    "vacation-per-year": 4
+};
+
 routes.get('/', (request, response) => {
     return response.render(views + "index");
 });
@@ -17,7 +26,7 @@ routes.get('/job/edit', (request, response) => {
 });
 
 routes.get('/profile', (request, response) => {
-    return response.render(views + "profile");
+    return response.render(views + "profile", { profile });
 });
 
 // Exporta as rotas
