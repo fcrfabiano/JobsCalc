14a28311ba064d4ed5a2fe9b3bffa7eae5f17883
Fabiano César <fcfabiano.cesar@icloud.com>
2021-04-08 00:54:35 -0300

    Template engine EJS

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,23 @@ const api = require('express');
 
 const routes = api.Router();
 
-const basePath = __dirname;
+const views = __dirname + "/views/";
 
 routes.get('/', (request, response) => {
-    return response.sendFile(`${basePath}/views/index.html`);
+    return response.render(views + "index");
 });
 
+routes.get('/job', (request, response) => {
+    return response.render(views + "job");
+});
+
+routes.get('/job/edit', (request, response) => {
+    return response.render(views + "job-edit");
+});
+
+routes.get('/profile', (request, response) => {
+    return response.render(views + "profile");
+});
+
+// Exporta as rotas
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ const routes = require('./routes');
 // Atribuindo função express à variável server
 const server = express();
 
+// Definindo a engine que o site vai usar
+server.set('view engine', 'ejs');
+
 // Middleware para configurar rotas estaticas
 server.use(express.static("public"));
 // Usar rotas vindas do routes.js
 server.use(routes);
-// Usar JSON para tratar as request/response
-server.use(express.json());
+
 // Servidor ouvindo na porta 3000
 server.listen(3000, () => {
     console.log("Servidor On-Line: [localhost:3000]");
